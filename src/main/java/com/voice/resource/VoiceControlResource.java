14957d2b6fe70b0a014c2ab69626774afeecd772
yizhangyin <yizhang.yin@gmail.com>
2019-01-29 20:36:34 +1300

    format

--- src/main/java/com/voice/resource/VoiceControlResource.java
+++ src/main/java/com/voice/resource/VoiceControlResource.java
@@ -5,6 +5,7 @@ package com.voice.resource;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,14 +36,14 @@ import com.voice.common.Text;
 @Path("/voice")
 public class VoiceControlResource {
 	private static final String PARAM_ID = "id";
-	
+
 	@GET
 	@Path("/v1/txt")
 	@Produces({ MediaType.APPLICATION_JSON })
 	public Payload<Text> getText(@QueryParam(PARAM_ID) String textId) throws IOException {
 		Payload<Text> payload = new Payload<>();
 		String command = "python " + AppConfig.getInstance().getPythonExeFilePath();
-		Process p = Runtime.getRuntime().exec(command + " " + textId );
+		Process p = Runtime.getRuntime().exec(command + " " + textId);
 		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
 		String ret = in.readLine();
 		Text data = new Text();
@@ -52,7 +53,7 @@ public class VoiceControlResource {
 		payload.setStatus(Response.Status.OK.getStatusCode());
 		return payload;
 	}
-	
+
 	@POST
 	@Path("/v1/save")
 	@Produces({ MediaType.APPLICATION_JSON })
@@ -61,33 +62,25 @@ public class VoiceControlResource {
 			@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
 		Payload<String> payload = new Payload<>();
 		String fileId = UUID.randomUUID().toString();
-		String fileName = AppConfig.getInstance().getVoiceFieFolderlPath() + "/" +fileId + ".wav";
+		String fileName = AppConfig.getInstance().getVoiceFieFolderlPath() + "/" + fileId + ".wav";
 		this.writeToFile(uploadedInputStream, fileName);
-	
+
 		payload.setData(fileId);
 		payload.setStatus(Response.Status.OK.getStatusCode());
 		return payload;
 	}
-	
-	private void writeToFile(InputStream uploadedInputStream,
-			String uploadedFileLocation) {
 
-			try {
-				OutputStream out = new FileOutputStream(new File(
-						uploadedFileLocation));
-				int read = 0;
-				byte[] bytes = new byte[1024];
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) throws IOException {
 
-				out = new FileOutputStream(new File(uploadedFileLocation));
-				while ((read = uploadedInputStream.read(bytes)) != -1) {
-					out.write(bytes, 0, read);
-				}
-				out.flush();
-				out.close();
-			} catch (IOException e) {
+		OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+		int read = 0;
+		byte[] bytes = new byte[1024];
 
-				e.printStackTrace();
-			}
-
+		out = new FileOutputStream(new File(uploadedFileLocation));
+		while ((read = uploadedInputStream.read(bytes)) != -1) {
+			out.write(bytes, 0, read);
 		}
+		out.flush();
+		out.close();
+	}
 }
