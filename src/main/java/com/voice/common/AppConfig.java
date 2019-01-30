d0eaecb0b10f1f48921503a505f8992fa8943ece
yizhangyin <yizhang.yin@gmail.com>
2019-01-30 18:55:06 +1300

    update client side audio record

--- src/main/java/com/voice/common/AppConfig.java
+++ src/main/java/com/voice/common/AppConfig.java
@@ -1,34 +1,18 @@
 package com.voice.common;
 
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-
 public class AppConfig {
 
-	private static final String CONFIG_FILE = "app.properties";
-
-	private static Configuration config;
-
 	private static AppConfig instance = new AppConfig();
-
-	static {
-		try {
-			config = new PropertiesConfiguration(CONFIG_FILE);
-		} catch (ConfigurationException e) {
-			throw new IllegalStateException(e);
-		}
-	}
 
 	public static AppConfig getInstance() {
 		return instance;
 	}
 	
 	public String getPythonExeFilePath() {
-		return config.getString("python.exe.file.path");
+		return "/Users/yizhangyin/Documents/python-ex/test.py";
 	}
 
 	public String getVoiceFieFolderlPath() {
-		return config.getString("voice.file.folder.path");
+		return "/Users/yizhangyin/Documents/wav";
 	}
 }
